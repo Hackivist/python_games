5732244abc160067c74195595c808493cd5f339f
KaKe <karthikeyanisaquizer@gmail.com>
2013-12-28 17:33:58 +0530

    Tentative final commit

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 <body>
 
-<h1>Random Number guess between 1 to 50</h1>
+<h1>Random Number guessing Game</h1>
 
 <p align=left>Hello everyone this is my first python game & project.I have developed it as a random number guessing game from 1 to 50.You can very well access the code and enjoy playing it in your system :) </p>
 
+<h2>Game Rules</h2>
+
+<ul>
+	<li>You have to give your name in the prompt</li>
+	<li>You have 6 chances to guess the number</li>
+	<li>If your number is too far away(more than 10(+ or -)) the prompt will say a too high or low hint </li>
+	<li>If your number is within the limit 10 , you will get a high or low hint</li>
+	<li>If your number is within the limit of 5 , you will get a very close hint</li>
+	<li>Guess the number within 6 number of tries :) </li>
+</ul>
+
+<p>
 </body>
--- guess_num.py
+++ guess_num.py
@@ -10,21 +10,34 @@ myName= raw_input()
 number = random.randint(1,50)
 
 print("Well, "+myName+ ",I am thinking of a number between 1 and 50.")
+print("Take a guess, try to find it in 6 guesses ;) ")
 
 while guessesTaken < 6:
-	print("Take a guess.")
 	guess = raw_input()
 	guess = int(guess)
 	
 	guessesTaken = guessesTaken +1;
 
-	if guess < number:
-		print("Your guess is too low.")
 	
-	if guess > number:
+	if guess > number+10:
 		print("Your guess is too high.")
+
+	if guess < number-10:
+		print("Your guess is too low.")
+
+	if guess < number+10 and guess > number + 5:
+		print("Your guess is high.")
+	
+	if guess > number-10 and guess < number -5:
+		print("Your guess is low.")
+	if guess <=  number+5 and guess >= number-5:
+		print("Your guess is very close") 
+	
+	print("Take another guess");
+
 	if guess == number:
 		break
+
 if  guess == number:
 	guessesTaken = str(guessesTaken)
 	print("Good Job, "+ myName +"! You guesses my number in "+ guessesTaken + " guesses!")
